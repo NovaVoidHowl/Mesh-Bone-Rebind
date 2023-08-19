e02a3a3e1a8451f715556c48c0b196ddfe6e6f94
NovaVoidHowl <me@novavoidhowl.com>
2023-08-19 17:17:08 +0100

    chore(code clean): apply pre-commit to files

--- README.md
+++ README.md
@@ -2,11 +2,10 @@
 
 ## Script Overview
 
-This script set is to allow rebinding of meshes to other armatures, note it does not and can not make incompatible armatures and meshes work with each other.  
+This script set is to allow rebinding of meshes to other armatures, note it does not and can not make incompatible armatures and meshes work with each other.
 
-The primary use case for this is to allow you to add extra items that are made for a given avatar base to it without having to constraint or stack armatures together.  
+The primary use case for this is to allow you to add extra items that are made for a given avatar base to it without having to constraint or stack armatures together.
 
-Note: For it to work the armatures of both the main avatar and the addon (cloths, armor etc.) must sufficiently match one and other, though the addon need not fully have all bones the avatar does.  
+Note: For it to work the armatures of both the main avatar and the addon (cloths, armor etc.) must sufficiently match one and other, though the addon need not fully have all bones the avatar does.
 
 Note: This does not work for addons which have extra bones (that the base avatar does not have)
-
--- Runtime/MeshBoneRebind.cs
+++ Runtime/MeshBoneRebind.cs
@@ -5,53 +5,50 @@ using UnityEngine;
 
 namespace uk.novavoidhowl.dev.meshbonerebind
 {
-    [ExecuteInEditMode]
-    public class MeshBoneRebind : MonoBehaviour
+  [ExecuteInEditMode]
+  public class MeshBoneRebind : MonoBehaviour
+  {
+    public Transform newArmature;
+    public string rootBoneName = "hips";
+
+    void Update() { }
+
+    [ContextMenu("Reassign Bones")]
+    public void Reassign()
     {
-        public Transform newArmature;
-        public string rootBoneName = "hips";
+      if (newArmature == null)
+      {
+        Debug.Log("No new armature assigned");
+        return;
+      }
 
-        void Update()
+      if (newArmature.Find(rootBoneName) == null)
+      {
+        Debug.Log("Root bone not found");
+        return;
+      }
+
+      Debug.Log("Reassigning bones");
+      SkinnedMeshRenderer rend = gameObject.GetComponent<SkinnedMeshRenderer>();
+      Transform[] bones = rend.bones;
+
+      rend.rootBone = newArmature.Find(rootBoneName);
+
+      Transform[] children = newArmature.GetComponentsInChildren<Transform>();
+
+      for (int i = 0; i < bones.Length; i++)
+      {
+        for (int a = 0; a < children.Length; a++)
         {
+          if (bones[i].name == children[a].name)
+          {
+            bones[i] = children[a];
+            break;
+          }
         }
-
-
-        [ContextMenu("Reassign Bones")]
-        public void Reassign()
-        {
-            if (newArmature == null)
-            {
-                Debug.Log("No new armature assigned");
-                return;
-            }
-
-            if (newArmature.Find(rootBoneName) == null)
-            {
-                Debug.Log("Root bone not found");
-                return;
-            }
-
-            Debug.Log("Reassigning bones");
-            SkinnedMeshRenderer rend = gameObject.GetComponent<SkinnedMeshRenderer>();
-            Transform[] bones = rend.bones;
-
-            rend.rootBone = newArmature.Find(rootBoneName);
-
-            Transform[] children = newArmature.GetComponentsInChildren<Transform>();
-
-            for (int i = 0; i < bones.Length; i++)
-            {
-                for (int a = 0; a < children.Length; a++)
-                {
-                    if (bones[i].name == children[a].name)
-                    {
-                        bones[i] = children[a];
-                        break;
-                    }
-                }
-            }
-            rend.bones = bones;
-        }
+      }
+      rend.bones = bones;
     }
+  }
 }
 #endif
